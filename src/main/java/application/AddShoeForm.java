ca494fb838005616d01563c8fe3ab83b9a79143e
Linus Boström <linus.bostrom@hotmail.com>
2024-04-11 23:55:48 +0200

    Started working on UI

---
+++ src/main/java/application/AddShoeForm.java
@@ -0,0 +1,16 @@
+package application;
+
+import javax.swing.*;
+
+public class AddShoeForm {
+    private JPanel panel1;
+    private JButton confirmButton;
+    private JPanel TopPanel;
+    private JPanel NamePanel;
+    private JPanel SizePanel;
+    private JPanel YearPanel;
+    private JPanel ButtonPanel;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+}
